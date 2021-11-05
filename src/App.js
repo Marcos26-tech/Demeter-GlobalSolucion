f31c22ff53874000dbddde6a8b53bee087febc27
Marcos26-tech <rm88267@fiap.com.br>
2021-11-05 09:39:32 -0300

    add ajuste

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-import { Routes } from './Routes';
+import Routes  from './Routes';
 import GlobalStyle from './Reset';
 
 function App() {
