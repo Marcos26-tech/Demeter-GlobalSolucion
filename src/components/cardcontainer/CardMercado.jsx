6825b7144e535da2560b79efc28e6e08392af511
Marcos26-tech <rm88267@fiap.com.br>
2021-11-11 17:39:49 -0300

    ajuste home

--- src/components/cardcontainer/CardMercado.jsx
+++ src/components/cardcontainer/CardMercado.jsx
@@ -23,6 +23,32 @@ const Produto = styled.div`
     margin: 0;
   }
 `;
+export const Button = styled.button`
+  margin: 2px 0 0 88%;
+  display: inline-block;
+  width: 10%;
+  padding: 5px 0;
+  text-align: center;
+  font-family: inherit;
+  font-size: 15px;
+  font-weight: 700;
+  color: rgb(255, 255, 255);
+  background-color: rgb(8, 8, 8);
+  border: 0;
+  border-radius: 35px;
+  box-shadow: 0 10px 10px rgba(0, 0, 0, 0.08);
+  cursor: pointer;
+  transition: all 0.25s cubic-bezier(0.02, 0.01, 0.47, 1);
+  &:hover {
+    box-shadow: 0 15px 15px rgba(0, 0, 0, 0.16);
+    transform: translate(0, -5px);
+  }
+  @media (max-width: 783px) {
+    font-size: 12px;
+    margin: 0 0 0 85%;
+    width: 19%;
+  }
+`;
 
 const selecionamercado = (id) => {
   fetch("/rest/" + id, {
---
+++ src/pages/pagehome/PageHomeCont.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import styled from "styled-components";
+import ShowSlide from "./SlideShow";
+
+const Section = styled.section`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  margin: 0;
+  padding: 2em;
+  align-items: center;
+`;
+
+const Div = styled.div`
+  width: 550px;
+  margin-left: 6em;
+  padding: 10px;
+  text-align: justify;
+  h1 {
+    font-size: 2em;
+    position: relative;
+    display: block;
+    margin: 10px;
+  }
+  p {
+    font-size: 1.2em;
+    position: relative;
+    display: block;
+    margin: 10px;
+  }
+`;
+
+const Divi = styled.div`
+  width: 700px;
+  height: 500px;
+  padding: 5px;
+  margin: 0 2rem 0 0;
+`;
+
+function PageHomeCont() {
+  return (
+    <>
+      <Section>
+        <Div>
+          <h1>UMA NOVA FORMA DE FAZER DOAÇOES PARA QUEM MAIS PRECISA!</h1>
+          <p>
+            O projeto consiste em que tanto os supermercados quanto as entidades
+            assistenciais possam se cadastrar para ter acesso às funcionalidades
+            da plataforma. Os supermercados podem cadastrar os produtos que
+            estejam com o prazo de validade próximo ao vencimento, informando as
+            características do produto, como nome, quantidade, localização e
+            data de validade. Já as entidades assistenciais podem verificar os
+            alimentos disponíveis em sua localidade, podendo reservar aqueles
+            que desejar, para posterior coleta no ponto informado pelo sistema.
+          </p>
+        </Div>
+        <Divi>
+          <ShowSlide />
+        </Divi>
+      </Section>
+      <Section>
+        <div>
+          <iframe
+            title="gif"
+            src="https://giphy.com/embed/gi7h31nLA9c6DVxpyn"
+            width="200"
+            height="150"
+          ></iframe>
+        </div>
+        <div>
+          <h2>Quem somos?</h2>
+          <h3>
+            Deméter nasceu com a finalidade é reduzir a distância entre a comida
+            abundante que seria desperdiçada e aqueles que a necessitam e
+            infelizmente, não tem acesso a ela, sobretudo pela condição
+            socioeconômica.
+          </h3>
+        </div>
+      </Section>
+      <Section>
+        <div>
+          <h2>Como funciona?</h2>
+          <h3>
+            pode ser uma img ilustrando o caminho que o usuário segue até chegar
+            ao objetivo. em construção
+          </h3>
+        </div>
+      </Section>
+      <Section>
+        <div>
+          <h2>Rank dos doadores que abraçaram a causa</h2>
+          <h3>
+            Afinal todos são bem vindos a contribuir com a nossa causa seja você
+            tambem um doador. em construção
+          </h3>
+        </div>
+      </Section>
+    </>
+  );
+}
+export default PageHomeCont;
